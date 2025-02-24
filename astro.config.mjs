5d4fb7b5efff43921344d02953b0ff1d8f0de81c
mohansky <38560439+mohansky@users.noreply.github.com>
2025-02-24 14:52:48 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig, envField } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), icon(), mdx()],
+
+  env: {
+    schema: {
+      RESEND_API_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        startsWith: "re_",
+        optional: false,
+      }),
+    },
+  },
+
+  adapter: netlify(),
+});
